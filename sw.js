b8998a0fa2bd36788fb35f8ec07299252de795ce
vaaneesh15 <vaaneesh@icloud.com>
2026-03-11 00:24:38 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
-  './icon.png',
+  './icon.svg',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-regular-400.woff2',
