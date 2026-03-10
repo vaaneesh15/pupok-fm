e7d5656f9760dd228b336d3648fbeef9d7e0414d
vaaneesh15 <vaaneesh@icloud.com>
2026-03-10 22:28:50 +0700

    обновлен второстепенный скрипт
    поддержка манифеста и иконки

--- sw.js
+++ sw.js
@@ -3,6 +3,8 @@ const CACHE_NAME = 'pupok-fm-v2';
 const urlsToCache = [
   './',
   './index.html',
+  './manifest.json',        // манифест
+  './icon.svg',             // иконка
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-solid-900.woff2',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/webfonts/fa-regular-400.woff2',
